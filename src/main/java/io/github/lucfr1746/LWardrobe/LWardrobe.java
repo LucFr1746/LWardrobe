8b116167314babef44102b6e920e347d9d05b51d
Thế Lực <LucFr.1746@gmail.com>
2024-04-10 14:24:47 +0700

    Added Metrics

--- src/main/java/io/github/lucfr1746/LWardrobe/LWardrobe.java
+++ src/main/java/io/github/lucfr1746/LWardrobe/LWardrobe.java
@@ -1,13 +1,20 @@
 package io.github.lucfr1746.LWardrobe;
 
+import io.github.lucfr1746.LWardrobe.Metrics.UpdateChecker;
+import io.github.lucfr1746.LWardrobe.Metrics.bStatsMetrics;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LWardrobe extends JavaPlugin {
 
+    public static Plugin getInstance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getInstance = this;
+    // Metrics
+        new UpdateChecker(this);
+        new bStatsMetrics(this, 21558);
     }
 
     @Override
