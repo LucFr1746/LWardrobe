669005236f031c07d2f737d9482c7c3cd38ef200
Thế Lực <LucFr.1746@gmail.com>
2024-04-10 10:56:13 +0700

    Base Plugin

---
+++ src/main/java/io/github/lucfr1746/LWardrobe/LWardrobe.java
@@ -0,0 +1,17 @@
+package io.github.lucfr1746.LWardrobe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LWardrobe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
